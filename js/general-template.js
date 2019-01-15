dc19fc34a1ad3d994d853fca0c2317c7ee553d5f
Ray Ivy <jrivy20@gmail.com>
2019-01-15 04:52:50 -0800

    Add files via upload

--- js/general-template.js
+++ js/general-template.js
@@ -1,5 +1,13 @@
 $(function(){
-	/* Nav Bar */
+	/* -------------------------- Nav Bar -------------------------- */
+
+	//Default button array
+	var navArray = ["Browse", "Albums", "Artists", "Advertising", "Pricing", "Contact"];
+	//Nav button loop
+		for(var i=0; i<navArray.length; i++){
+			navArray[i] = new navButton(navArray[i]);
+		}
+
 	var navBar = document.createElement('nav');
 		navBar.setAttribute('id', 'genNav');
 		navBar.setAttribute('class', 'navbar navbar-expand-lg fixed-top');
@@ -22,5 +30,28 @@ $(function(){
 			collapseContent.setAttribute('id', 'navbarSupportedContent');
 			var buttonList = document.createElement('ul');
 				buttonList.setAttribute('class', 'navbar-nav mr-4');
-				
+				for(var k=0; k<navArray.length; k++){
+					var listItem = document.createElement('li');
+						listItem.setAttribute('class', 'nav-item');
+						var listButton = document.createElement('a');
+							listButton.setAttribute('class', 'nav-link');
+							listButton.setAttribute('href', navArray[k].link);
+							$(listButton).text(navArray[k].label);
+						listItem.append(listButton);
+					buttonList.append(listItem);
+				}
+			collapseContent.append(buttonList);
+		navBar.append(brand, collapseButton, collapseContent);
+	$('#bod').append(navBar);
+	
+	//Nav button object function
+	function navButton(label){
+		this.label = label;
+		this.link = "navLinks/" + label ;
+	}
+	//Nav button management
+	function customNav(buttonID, newLabel){
+		navArray[buttonID] = newLabel;
+	}
+
 })
