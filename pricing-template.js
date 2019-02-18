980526d8732bb5c201f848533ec0672d1a8ae5c6
Ray Ivy <jrivy20@gmail.com>
2019-02-18 06:12:23 -0800

    Delete pricing-template.js

--- pricing-template.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	/* -------------------------- Nav Bar -------------------------- */
-	$('#PricingButton').addClass('active');
-	/* ------------------------ End Nav Bar ------------------------ */
-	var bod = $('#bod');
-	setPricing(bod);
-})
